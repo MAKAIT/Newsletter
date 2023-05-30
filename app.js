40fa5a15cd672dd0f5abfd954b07dedf9413caf5
MAKAIT <makhloufaitlaziz@gmail.com>
2023-05-30 15:34:04 +0200

    Commit first version

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const https = require("https")
+const { METHODS } = require("http")
+
+const app = express()
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/", (req, res)=>{
+    const FName = req.body.fname
+    const LName = req.body.lname
+    const email = req.body.email
+
+    const data ={
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: FName,
+                    LNAME: LName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data)
+    const url = "https://us6.api.mailchimp.com/3.0/lists/646ec8927b"
+    const options = {
+        method: "POST",
+        auth: "MAKAIT:f9b3367e8711607652c49a7a8c2e2abc"
+    }
+
+    const request = https.request(url, options, (response)=>{
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html")
+        }else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+        response.on("data", (data)=>{
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData)
+    request.end()
+})
+
+app.post("/failure", (req, res)=>{
+    res.redirect("/")
+})
+
+app.listen("3000", ()=>{
+    console.log("Listening on 3000");
+})
+
+// API KEY
+// f9b3367e8711607652c49a7a8c2e2abc-us6
+// 646ec8927b
